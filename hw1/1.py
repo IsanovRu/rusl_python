2de73f9630ad1edf785c887d2dceec0b77ee64aa
ruslan <to@ufasg.com>
2021-02-08 15:11:37 +0500

    Дз1 востановленно

--- hw1/1.py
+++ hw1/1.py
@@ -1,1 +1,8 @@
-print('1')
+a = 'hello'
+b = 'world!'
+print(f"{a}, {b}")
+num1 = int(input('Введите число - '))
+num2 = int(input('Введите число - '))
+print(f"Вы ввели числа {num1} и {num2}")
+word = input("Enter any word: ")
+print(f"{word} - отличный выбор!")
